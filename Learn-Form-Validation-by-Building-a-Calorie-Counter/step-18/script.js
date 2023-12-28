ba06230b5e50476dd27fa5e227fff028818551e2
Thiago Tavares <Thiagoff2019@hotmail.com>
2023-12-28 16:51:05 -0300

    step-18

---
+++ Learn-Form-Validation-by-Building-a-Calorie-Counter/step-18/script.js
@@ -0,0 +1,16 @@
+// Step 18
+// Even though you set an input element to be a number, JavaScript receives a string value. You need to write a function to clean the string value and ensure you have a number.
+
+// Start by declaring a cleanInputString function that takes a str argument.
+const calorieCounter = document.getElementById('calorie-counter')
+const budgetNumberInput = document.getElementById('budget')
+const entryDropdown = document.getElementById('entry-dropdown')
+const addEntryButton = document.getElementById('add-entry')
+const clearButton = document.getElementById('clear')
+const output = document.getElementById('output')
+let isError = false
+
+function cleanInputString(str){
+    
+
+}
