24851a9de2bffddae9041596b0377ec6a0d087d2
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-01-02 11:00:23 -0300

    step-49

---
+++ Learn-Form-Validation-by-Building-a-Calorie-Counter/step-49/script.js
@@ -0,0 +1,39 @@
+// Step 49
+// To see your new HTML content for the targetInputContainer, you will need to use the innerHTML property.
+
+// The innerHTML property sets or returns the HTML content inside an element. Here is an example of how to use it:
+
+// <div id="container">
+//   <p>Example paragraph</p>
+// </div>
+// const container = document.getElementById("container");
+// container.innerHTML += `
+//   <p>adding new content</p>
+// `;
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str){
+    const regex = /[+-\s]/g;
+    return str.replace(regex,'')
+}
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i;
+    return str.match(regex)
+}
+function addEntry(){
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>
+    `
+    targetInputContainer.innerHTML += HTMLString
+}
