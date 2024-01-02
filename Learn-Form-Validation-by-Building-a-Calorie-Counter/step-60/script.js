0fc15ec0bf56b608b60f8b96209cabe4f9d0a163
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-01-02 11:59:23 -0300

    step-60

---
+++ Learn-Form-Validation-by-Building-a-Calorie-Counter/step-60/script.js
@@ -0,0 +1,49 @@
+// Step 60
+// Remember that your isInvalidInput function returns String.match, which is an array of matches or null if no matches are found.
+
+// In JavaScript, values can either be truthy or falsy. A value is truthy if it evaluates to true when converted to a Boolean. A value is falsy if it evaluates to false when converted to a Boolean. null is an example of a falsy value.
+
+// You need to check if invalidInputMatch is truthy – you can do this by passing the variable directly to your if condition (without a comparison operator). Here's an example of checking the truthiness of helloWorld.
+
+// if (helloWorld) {
+
+// }
+// Add an if statement that checks if invalidInputMatch is truthy.
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str){
+    const regex = /[+-\s]/g;
+    return str.replace(regex,'')
+}
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i;
+    return str.match(regex)
+}
+function addEntry(){
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>
+    `
+    targetInputContainer.insertAdjacentHTML('beforeend',HTMLString) 
+}
+
+addEntryButton.addEventListener('click',addEntry)
+
+function getCaloriesFromInputs(list){
+        let calories = 0
+    for(let i = 0; i < list.length; i++){
+        const currVal = cleanInputString(list[i].value)
+       let invalidInputMatch =isInvalidInput(currVal)
+       if(invalidInputMatch){}
+    }
+}
