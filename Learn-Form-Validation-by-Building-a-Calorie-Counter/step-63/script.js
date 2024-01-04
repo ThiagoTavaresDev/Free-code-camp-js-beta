5ce004bb82bcd70221d5c81c970e810d6345cc47
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-01-04 15:00:28 -0300

    step-63

---
+++ Learn-Form-Validation-by-Building-a-Calorie-Counter/step-63/script.js
@@ -0,0 +1,53 @@
+// Step 63
+// Remember that return ends the execution of a function. After your if block, you need to handle the logic for when the input is valid. Because your if statement returns a value, you do not need an else statement.
+
+// Use the addition assignment operator to add currVal to your calories total. You'll need to use the Number constructor to convert currVal to a number.
+
+// The Number constructor is a function that converts a value to a number. If the value cannot be converted, it returns NaN which stands for "Not a Number".
+
+// Here is an example:
+
+// Number('10'); // returns the number 10
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str){
+    const regex = /[+-\s]/g;
+    return str.replace(regex,'')
+}
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i;
+    return str.match(regex)
+}
+function addEntry(){
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>
+    `
+    targetInputContainer.insertAdjacentHTML('beforeend',HTMLString) 
+}
+
+addEntryButton.addEventListener('click',addEntry)
+
+function getCaloriesFromInputs(list){
+        let calories = 0
+    for(let i = 0; i < list.length; i++){
+        const currVal = cleanInputString(list[i].value)
+       let invalidInputMatch =isInvalidInput(currVal)
+       if(invalidInputMatch){
+        alert(`Invalid Input: ${invalidInputMatch[0]}`)
+        isError = true
+        return null
+       }
+        calories += Number(currVal)
+    }
+}
