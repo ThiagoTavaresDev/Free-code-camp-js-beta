a30b1b4f199cfb72b740ad2100ddf3c829d4f5b5
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-01-04 15:21:40 -0300

    step-70

---
+++ Learn-Form-Validation-by-Building-a-Calorie-Counter/step-70/script.js
@@ -0,0 +1,59 @@
+// Step 70
+// Now that you have your lists of elements, you can pass them to your getCaloriesFromInputs function to extract the calorie total.
+
+// Declare a breakfastCalories variable, and assign it the result of calling getCaloriesFromInputs with breakfastNumberInputs as the argument.
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str){
+    const regex = /[+-\s]/g;
+    return str.replace(regex,'')
+}
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i;
+    return str.match(regex)
+}
+function addEntry(){
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>
+    `
+    targetInputContainer.insertAdjacentHTML('beforeend',HTMLString) 
+}
+
+addEntryButton.addEventListener('click',addEntry)
+
+function getCaloriesFromInputs(list){
+        let calories = 0
+    for(let i = 0; i < list.length; i++){
+        const currVal = cleanInputString(list[i].value)
+       let invalidInputMatch =isInvalidInput(currVal)
+       if(invalidInputMatch){
+        alert(`Invalid Input: ${invalidInputMatch[0]}`)
+        isError = true
+        return null
+       }
+        calories += Number(currVal)
+
+    }
+    return calories
+}
+function calculateCalories(e){
+    e.preventDefault()
+    isError = false
+    let breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]')
+    let lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]')
+    let dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]')
+    let snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]')
+    let exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]')
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs)
+}
