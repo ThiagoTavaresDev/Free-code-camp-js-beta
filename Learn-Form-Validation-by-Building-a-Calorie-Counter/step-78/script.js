9c612b83e13e0c21bc20dcf1e50fe49016a4a362
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-01-04 15:49:48 -0300

    step-78

---
+++ Learn-Form-Validation-by-Building-a-Calorie-Counter/step-78/script.js
@@ -0,0 +1,69 @@
+// Step 78
+// You need to construct the HTML string that will be displayed in the output element. Start by assigning an empty template literal to the innerHTML property of the output element.
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('#budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str){
+    const regex = /[+-\s]/g;
+    return str.replace(regex,'')
+}
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i;
+    return str.match(regex)
+}
+function addEntry(){
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>
+    `
+    targetInputContainer.insertAdjacentHTML('beforeend',HTMLString) 
+}
+
+addEntryButton.addEventListener('click',addEntry)
+
+function getCaloriesFromInputs(list){
+        let calories = 0
+    for(let i = 0; i < list.length; i++){
+        const currVal = cleanInputString(list[i].value)
+       let invalidInputMatch =isInvalidInput(currVal)
+       if(invalidInputMatch){
+        alert(`Invalid Input: ${invalidInputMatch[0]}`)
+        isError = true
+        return null
+       }
+        calories += Number(currVal)
+
+    }
+    return calories
+}
+function calculateCalories(e){
+    e.preventDefault()
+    isError = false
+    let breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]')
+    let lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]')
+    let dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]')
+    let snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]')
+    let exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]')
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs)
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs)
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs)
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs)
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs)
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput])
+    if(isError){
+        return
+    }
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories + exerciseCalories
+    const remainingCalories =  budgetCalories - consumedCalories  +  exerciseCalories
+    const surplusOrDeficit = remainingCalories >= 0 ? 'Surplus' : 'Deficit'
+    output.innerHTML = ``
+}
